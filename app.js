853ffa4b49b4ef5ed090368a13b9f30b437cf1f5
Shane Welsh <shanemwelsh@gmail.com>
2017-01-20 11:22:56 -0500

    Add scraper for releases - incomplete

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var http = require('http');
 var Artist = require('./models/artist');
 
 function processArtists(artists) {
+    var artist = new Artist();
+    var temp = null;
+    
     // If webservice return no results, skip this
     if(!artists) {
         return;
@@ -9,8 +12,8 @@ function processArtists(artists) {
 
     var i = artists.length;
     while(i--) {
-        var temp = artists[i];
-        var artist = new Artist({
+        temp = artists[i];
+        artist = new Artist({
             id: temp.id,
             name: temp.name,
             created: Date()
@@ -35,27 +38,45 @@ function processArtists(artists) {
         if(temp['life-span']) {
             artist['life-span'] = temp['life-span'];
         }
+        if(temp.disambiguation) {
+            artist.disambiguation = temp.disambiguation;
+        }
+        if(temp.aliases) {
+            artist.disambiguation = temp.aliases;
+        }
 
         // Save the artist into the database
         artist.save((err,tmp) => {
-            if (err) throw err;
+            if (err) {
+                if(err.code == 11000) {
+                    console.log('[ERROR][DUP ARTIST]', tmp.name);
+                    return;
+                }
+                throw err;
+            }
+            console.log('[ INFO][SAVED]', tmp.name);
         });
     }
 }
 
-function main() {
-    var offset = 0;
+function processReleases() {
 
+}
+
+function scrapeArtists() {
+    var offset = 0;
+    
     var interval = setInterval(() => {
         var options = {
             host: 'musicbrainz.org',
             port: '80',
+            // Webservice limit of 100 results per query (default of 25)
             path: '/ws/2/artist/?query=tag:punk&limit=100&offset=' + offset + '&fmt=json',
             method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json; charset=utf-8',
-                    'User-Agent': 'Database Builder v0.1.0 | PunkDB | www.punkdb.com | thepunkdb@gmail.com'
-                }
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8',
+                'User-Agent': 'Database Builder v0.1.0 | PunkDB | www.punkdb.com | thepunkdb@gmail.com'
+            }
         };
 
         var callback = (res) => {
@@ -76,14 +97,67 @@ function main() {
                 
                 // Termination of loop
                 if(offset > raw.count) {
-                    process.exit();
+                    clearInterval(interval);
                 }
             });
         };
 
         http.request(options, callback).end();
 
-    }, 20000); // Using generous timeout to be friendly on musicbrainz webservice
+    }, 5000); // Using generous timeout to be friendly on musicbrainz webservice
+}
+
+function scrapeReleases() {
+    
+    Artist.find({}).select({id: 1, name: 1}).exec((err, result) => {
+        var offset = 0;
+        var i = 0;
+        var interval = setInterval(() => {
+            var options = {
+                host: 'musicbrainz.org',
+                port: '80',
+                // TODO: Need to correct the request URL to have dynamic artist name based off
+                // database query
+                path: '/ws/2/release/?query=artist:' + result[i++].name + '&limit=100&offset=' + offset + '&fmt=json',
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json; charset=utf-8',
+                    'User-Agent': 'Database Builder v0.1.0 | PunkDB | www.punkdb.com | thepunkdb@gmail.com'
+                }
+            }
+
+            var callback = (res) => {
+                var msg = '';
+
+                res.setEncoding('utf8');
+                res.on('data', (chunk) => {
+                    msg += chunk;
+                });
+                res.on('end', () => {
+                    var raw = JSON.parse(msg);
+
+                    offset += 100;
+
+                    processReleases(raw['releases']);
+
+                    if(offset > raw.count) {
+                        clearInterval(interval);
+                    }
+                });
+            };
+
+            http.request(options, callback).end();
+
+        }, 5000); // Using generous timeout to be friendly on musicbrainz webservice
+    });
+}
+
+function main() {
+    //scrapeArtists();
+    scrapeReleases();
+    // TODO:
+    // scrapeLabel();
+    // scrapeReleaseGroup(); --Maybe
 }
 
 main();
